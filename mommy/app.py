1d145c6dae765eff7e15365f00ec9ceec47070a2
Felipe Alves <118228332+alvesgfelipe@users.noreply.github.com>
2022-11-25 17:04:08 -0300

    ✨ Adicionar suporte para testes

--- mommy/app.py
+++ mommy/app.py
@@ -2,9 +2,15 @@ from flask import Flask
 from models import db
 
 
-def create_app() -> Flask:
+def create_app(usar_pytest: bool = False) -> Flask:
     app: Flask = Flask(__name__)
-    app.config.from_prefixed_env()
+
+    if usar_pytest:
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+        app.config["TESTING"] = True
+    else:
+        app.config.from_prefixed_env()
 
     db.init_app(app)
 
